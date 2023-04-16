129912b177202f13f43f3672fe8ba03f3411478f
Franz Flores <franz.flores@clipp.app>
2023-04-15 22:40:13 -0500

    Funciones anónimas y ámbito

---
+++ 04_functions/04_scope/main.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function helloWorld(text){
+	var message = "Texto dentro de la función";
+
+	console.log(text);
+	console.log(number.toString());
+	console.log(message);
+}
+
+var number = 12;
+var text = "Hola mundo soy una variable global";
+helloWorld(text);
